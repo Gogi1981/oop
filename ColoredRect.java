92ab6204d8969334ba50a3fb4502329c5867bd9b
Gogi1981 <43212154+Gogi1981@users.noreply.github.com>
2019-05-28 19:11:39 +0300

    14

--- ColoredRect.java
+++ ColoredRect.java
@@ -3,26 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package lab3;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /**
  *
- * @author Гоги
+ * @author Is_17_1
  */
 import java.text.MessageFormat;
 
 public class ColoredRect extends DrawRect {
 
     private String inColor;
-
-    public void draw(String outColor, String inColor) {
-        draw(outColor);
+public ColoredRect(int x1, int y1, int x2, int y2, String outColor, String inColor) {
+        super(x1, y1, x2, y2, outColor);
         this.inColor = inColor;
     }
-
-    @Override
-    public String toString() {
-        return MessageFormat.format("{0}, in: {1}", super.toString(), inColor);
+     public ColoredRect() {
+        this(0, 0, 0, 0, "red","green");
     }
-
+     
+    public void draw(String outColor, String inColor) {
+        super.draw(outColor);
+        this.inColor = inColor;
+         System.out.println("Цвет внутрений:  "+inColor);
+    }
+    
+    
 }
