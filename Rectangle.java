67fafd822bcb38baa2b7e1dcb07fc0a6844be09c
Gogi1981 <43212154+Gogi1981@users.noreply.github.com>
2019-05-28 19:13:56 +0300

    1

--- Rectangle.java
+++ Rectangle.java
@@ -3,21 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package lab3;
 
 /**
  *
- * @author Гоги
+ * @author Is_17_1
  */
 import java.text.MessageFormat;
 
+
+/**
+ * 
+ * 
+ * @author Is_17_1  
+ */
 public class Rectangle {
 
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
-
+    int x1,y1,x2,y2;
+    
     public Rectangle(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
@@ -36,18 +39,21 @@ public class Rectangle {
     public void move(int dx, int dy) {
         x1 += dx;
         x2 += dx;
-
         y1 += dy;
         y2 += dy;
     }
-
+/**
+ * 
+ * @param other fghjkvfbhmfgjm
+ * @return hbj,ghjlапроленг
+ */
     public Rectangle minSquare(Rectangle other) {
         return this.square() < other.square() ? this : other;
     }
 
     @Override
     public String toString() {
-        return MessageFormat.format("x1: {0}, y1: {1}, x2: {2}, y2: {3}", x1, y1, x2, y2);
+        return ("x1: "+ x1 +" y1: "+ y1 +" x2: "+ x2 +" y2: " + y2);
     }
 
     private int square() {
@@ -55,3 +61,4 @@ public class Rectangle {
     }
 
 }
+
